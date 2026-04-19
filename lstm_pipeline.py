1111872c3df821a1617dc3e53121f26bb7a2b8d3
ecw70574 <131731011+ecw70574@users.noreply.github.com>
2026-04-19 15:38:05 -0400

    fixed survival groups logic and naming of csvs for aggregation script feeding into lstm pipeline script

--- lstm_pipeline.py
+++ lstm_pipeline.py
@@ -1,8 +1,11 @@
 import pandas as pd
+import numpy as np
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 # 1. load data
-data = pd.read_csv("lstm_preprocessed_data.csv")
+# reading in resulting CSV from aggregate_and_build.py script 
+# data = pd.read_csv("lstm_preprocessed_data.csv")
+data = pd.read_csv("all_years_aggregated.csv")
 
 # 2. group id
 data["group_id"] = (
@@ -19,7 +22,9 @@ group_survival = (
     .reset_index(name='year_count')
 )
 
-surviving_groups = group_survival[group_survival["year_count"] >= 5]["group_id"]
+# making sure groups are consistent between all 8 years 
+# true time series will track same groups over time 
+surviving_groups = group_survival[group_survival["year_count"] >= 8]["group_id"]
 
 model_data = data[data["group_id"].isin(surviving_groups)].copy()
 
@@ -30,27 +35,36 @@ model_data = model_data.sort_values(["group_id", "year"])
 model_data = model_data.fillna(0)
 
 # 6. features
+# everything except the group id, year, and target variable 
 feature_cols = [
     c for c in model_data.columns
     if c not in ["group_id", "year", "did_vote_1"]
 ]
 
-# 7. sequences
+# 7. buidling sequences for each group
 X_seq, y_seq = [], []
 
 for gid, g in model_data.groupby("group_id"):
     g = g.sort_values("year")
 
-    X_seq.append(g[feature_cols].values)
-    y_seq.append(g["did_vote_1"].values)
+    X_seq.append(g[feature_cols].values) # creating a sequence of features per group 
+    y_seq.append(g["did_vote_1"].values) # creating a sequence of target feature value per group
+# each group now has a sequence of X and y values  
 
 # 8. padding
-X = pad_sequences(X_seq, dtype="float32", padding="post")
-y = pad_sequences(y_seq, dtype="float32", padding="post")
+# add zeros at end if uneven time series lengths between groups
+# SHOULDN"T NEED IF WE ALREADY HAVE USED THE GROUP SURVIVAL 
+# already ensured same 8 years are non-missing for each group 
+# X = pad_sequences(X_seq, dtype="float32", padding="post") 
+# y = pad_sequences(y_seq, dtype="float32", padding="post")
+X = np.array(X_seq)
+y = np.array(y_seq)
+
 
 # split into training and testing 
 
 # train on years <- 2018, validate on 2020-2022, test on 2024
-train_data = model_data[model_data["year"] <= 18]
-val_data   = model_data[(model_data["year"] > 18) & (model_data["year"] <= 22)]
-test_data  = model_data[model_data["year"] == 24]
+train_data = model_data[model_data["year"] <= 18] # for training
+val_data   = model_data[(model_data["year"] > 18) & (model_data["year"] <= 22)] # for tuning/selection
+# after final model has been tuned/optimized, avoid data leakage
+test_data  = model_data[model_data["year"] == 24] # for testing performance on unseen observations 
